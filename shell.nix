4da25c980d43b17fc512eda4229b5a65ca4c6cde
Shane Pearman <spearman@gitlab.com>
2026-03-08 19:46:39 +0100

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@ mkShell {
   buildInputs = [
     clang
     cmake
-    cudaPackages.cuda_nvcc
+    cudaPackages.cudatoolkit
     pipenv
     pkg-config
     pre-commit
@@ -19,9 +19,6 @@ mkShell {
   LD_LIBRARY_PATH = lib.makeLibraryPath [
     (lib.getLib pkgs.stdenv.cc.cc)  # required for numpy
     (lib.getLib pkgs.llvm)          # libLLVM.so
-    #ocl-icd                         # libOpenCL.so
-    #linuxPackages.nvidia_x11        # libcuda.so
-    cudaPackages.cudatoolkit        # libnvrtc.so
   ];
   CUDA_PATH = "${linuxPackages.nvidia_x11}/lib/libcuda.so";
   OPENCL_PATH = "${ocl-icd}/lib/libOpenCL.so";
@@ -29,5 +26,6 @@ mkShell {
     mkdir -p tmp
     export TMPDIR="$(pwd)/tmp"
     export CC="clang"
+    pipenv shell
   '';
 }
